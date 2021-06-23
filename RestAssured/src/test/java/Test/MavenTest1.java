f362a6c08e24b62e6c399ff970e5867856599d55
01217P744 <01217P744@9.206.169.28>
2021-06-23 12:50:55 +0530

    Trying for 2nd time from eclipse

---
+++ RestAssured/src/test/java/Test/MavenTest1.java
@@ -0,0 +1,21 @@
+package Test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+
+public class MavenTest1 {
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) {
+	  
+	  System.out.println(n);
+	  System.out.println(s);
+  }
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+}
